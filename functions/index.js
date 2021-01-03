e233218b6185c76935b592c38785492faf5a305e
Arvid <arvid.pilhall@me.com>
2021-01-03 17:22:13 +0100

    Added new function to get groceries

--- functions/index.js
+++ functions/index.js
@@ -1,100 +1,130 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors({ origin: true }));
 
 var serviceAccount = require("./auth.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://fridge-23daa..firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fridge-23daa..firebaseio.com",
 });
 const db = admin.firestore();
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send('Hello World!');
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("Hello World!");
 });
 
+app.post("/api/user", (req, res) => {
+  (async () => {
+    try {
+      await db
+        .collection("users")
+        .doc("/" + req.body.id + "/")
+        .create({ user: req.body.item });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
-app.post('/api/user', (req, res) => {
-    (async () => {
-        try {
-            await db.collection('users').doc('/' + req.body.id + '/')
-                .create({ user: req.body.item });
-            return res.status(200).send();
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
+app.get("/api/user/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("users").doc(req.params.id);
+      let item = await document.get();
+      let response = item.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+app.get("/api/users", (req, res) => {
+  (async () => {
+    try {
+      let query = db.collection("users");
+      let response = [];
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+        for (let doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            user: doc.data().user,
+          };
+          response.push(selectedItem);
         }
-    })();
+        return;
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
-app.get('/api/user/:id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('users').doc(req.params.id);
-            let item = await document.get();
-            let response = item.data();
-            return res.status(200).send(response);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
+app.get("/api/user/:userId/fridge/:fridgeId/groceries", (req, res) => {
+  (async () => {
+    try {
+      let query = db
+        .collection("users")
+        .doc(req.params.userId)
+        .collection("fridges")
+        .doc(req.params.fridgeId)
+        .collection("groceries")
+      let response = [];
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+        for (let doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            name: doc.data().name,
+            bestBefore: doc.data().bestBefore.toDate()
+          };
+          response.push(selectedItem);
         }
-    })();
+        return;
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
-app.get('/api/users', (req, res) => {
-    (async () => {
-        try {
-            let query = db.collection('users');
-            let response = [];
-            await query.get().then(querySnapshot => {
-                let docs = querySnapshot.docs;
-                for (let doc of docs) {
-                    const selectedItem = {
-                        id: doc.id,
-                        user: doc.data().user
-                    };
-                    response.push(selectedItem);
-                }
-                return;
-            });
-            return res.status(200).send(response);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
+app.put("/api/user/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("users").doc(req.params.id);
+      await document.update({
+        user: req.body.user,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
-app.put('/api/user/:id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('users').doc(req.params.id);
-            await document.update({
-                user: req.body.user
-            });
-            return res.status(200).send();
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
+app.delete("/api/user/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("users").doc(req.params.id);
+      await document.delete();
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
-
-app.delete('/api/user/:id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('users').doc(req.params.id);
-            await document.delete();
-            return res.status(200).send();
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
-
 
 exports.app = functions.https.onRequest(app);
