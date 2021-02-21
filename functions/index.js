fae0e1b64d8904a07fa707152c3aad72bfbfa7e5
Arvid <arvid.pilhall@me.com>
2021-02-21 14:25:34 +0100

    Added and commented out functions

--- functions/index.js
+++ functions/index.js
@@ -12,11 +12,81 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-app.get("/hello-world", (req, res) => {
-  return res.status(200).send("Hello World!");
-});
+// app.get("/hello-world", (req, res) => {
+//   return res.status(200).send("Hello World!");
+// });
 
-app.post("/api/user", (req, res) => {
+// app.post("/api/user", (req, res) => {
+//   (async () => {
+//     if (!req.headers.authorization) {
+//       return res.status(403).json({ error: "No credentials sent!" });
+//     }
+//     try {
+//       admin
+//         .auth()
+//         .verifyIdToken(req.headers.authorization)
+//         .then(async (decodedToken) => {
+//           const uid = decodedToken.uid;
+//           try {
+//             await db
+//               .collection("users")
+//               .doc("/" + uid + "/")
+//               .create({ user: "hehehe" });
+//             return res.status(200).send();
+//           } catch (error) {
+//             console.log(error);
+//             return res.status(500).send(error);
+//           }
+//         })
+//         .catch((error) => {
+//           return res.status(401).json({ error: error });
+//         });
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
+
+// app.get("/api/user/:id", (req, res) => {
+//   (async () => {
+//     try {
+//       const document = db.collection("users").doc(req.params.id);
+//       let item = await document.get();
+//       let response = item.data();
+//       return res.status(200).send(response);
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
+
+// app.get("/api/users", (req, res) => {
+//   (async () => {
+//     try {
+//       let query = db.collection("users");
+//       let response = [];
+//       await query.get().then((querySnapshot) => {
+//         let docs = querySnapshot.docs;
+//         for (let doc of docs) {
+//           const selectedItem = {
+//             id: doc.id,
+//             user: doc.data().user,
+//           };
+//           response.push(selectedItem);
+//         }
+//         return;
+//       });
+//       return res.status(200).send(response);
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
+
+app.get("/api/user/fridge/:fridgeId", (req, res) => {
   (async () => {
     if (!req.headers.authorization) {
       return res.status(403).json({ error: "No credentials sent!" });
@@ -27,84 +97,30 @@ app.post("/api/user", (req, res) => {
         .verifyIdToken(req.headers.authorization)
         .then(async (decodedToken) => {
           const uid = decodedToken.uid;
-          try {
-            await db
-              .collection("users")
-              .doc("/" + uid + "/")
-              .create({ user: "hehehe" });
-            return res.status(200).send();
-          } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-          }
-        })
-        .catch((error) => {
-          return res.status(401).json({ error: error });
-        });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
 
-app.get("/api/user/:id", (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection("users").doc(req.params.id);
-      let item = await document.get();
-      let response = item.data();
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+          let response = {};
 
-app.get("/api/users", (req, res) => {
-  (async () => {
-    try {
-      let query = db.collection("users");
-      let response = [];
-      await query.get().then((querySnapshot) => {
-        let docs = querySnapshot.docs;
-        for (let doc of docs) {
-          const selectedItem = {
-            id: doc.id,
-            user: doc.data().user,
-          };
-          response.push(selectedItem);
-        }
-        return;
-      });
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+          //Get fridge data
+          let queryFridge = db
+            .collection("users")
+            .doc(uid)
+            .collection("fridges")
+            .doc(req.params.fridgeId);
+          await queryFridge.get().then((querySnapshot) => {
+            response = querySnapshot.data();
+            return;
+          });
 
-app.get("/api/user/fridge/:fridgeId/groceries", (req, res) => {
-  (async () => {
-    if (!req.headers.authorization) {
-      return res.status(403).json({ error: "No credentials sent!" });
-    }
-    try {
-      admin
-        .auth()
-        .verifyIdToken(req.headers.authorization)
-        .then(async (decodedToken) => {
-          const uid = decodedToken.uid;
-          let query = db
+          //Get groceries for fridge
+          let groceries = [];
+
+          let queryGroceries = db
             .collection("users")
             .doc(uid)
             .collection("fridges")
             .doc(req.params.fridgeId)
             .collection("groceries");
-          let response = [];
-          await query.get().then((querySnapshot) => {
+          await queryGroceries.get().then((querySnapshot) => {
             let docs = querySnapshot.docs;
             for (let doc of docs) {
               const selectedItem = {
@@ -112,17 +128,18 @@ app.get("/api/user/fridge/:fridgeId/groceries", (req, res) => {
                 name: doc.data().name,
                 bestBefore: doc.data().bestBefore.toDate(),
               };
-              response.push(selectedItem);
+              groceries.push(selectedItem);
             }
             return;
           });
+          response.groceries = groceries;
+
           return res.status(200).send(response);
         })
         .catch((error) => {
           return res.status(401).json({ error: error });
         });
     } catch (error) {
-      console.log(error);
       return res.status(500).send(error);
     }
   })();
@@ -152,7 +169,6 @@ app.post("/api/user/fridge/:fridgeId/grocery", (req, res) => {
               });
             return res.status(200).send();
           } catch (error) {
-            console.log(error);
             return res.status(500).send(error);
           }
         })
@@ -160,7 +176,6 @@ app.post("/api/user/fridge/:fridgeId/grocery", (req, res) => {
           return res.status(401).json({ error: error });
         });
     } catch (error) {
-      console.log(error);
       return res.status(500).send(error);
     }
   })();
@@ -177,15 +192,18 @@ app.get("/api/user/fridges", (req, res) => {
         .verifyIdToken(req.headers.authorization)
         .then(async (decodedToken) => {
           const uid = decodedToken.uid;
-          let query = db.collection("users").doc(uid).collection("fridges");
+          let query = await db
+            .collection("users")
+            .doc(uid)
+            .collection("fridges");
           let response = [];
+
           await query.get().then((querySnapshot) => {
             let docs = querySnapshot.docs;
             for (let doc of docs) {
               const selectedItem = {
                 id: doc.id,
                 name: doc.data().name,
-                bestBefore: doc.data().bestBefore.toDate(),
               };
               response.push(selectedItem);
             }
@@ -197,23 +215,61 @@ app.get("/api/user/fridges", (req, res) => {
           return res.status(401).json({ error: error });
         });
     } catch (error) {
-      console.log(error);
       return res.status(500).send(error);
     }
   })();
 });
 
-app.delete("/api/user/:id", (req, res) => {
+app.get("/api/user/fridge", (req, res) => {
   (async () => {
+    if (!req.headers.authorization) {
+      return res.status(403).json({ error: "No credentials sent!" });
+    }
     try {
-      const document = db.collection("users").doc(req.params.id);
-      await document.delete();
-      return res.status(200).send();
+      admin
+        .auth()
+        .verifyIdToken(req.headers.authorization)
+        .then(async (decodedToken) => {
+          const uid = decodedToken.uid;
+          let query = await db
+            .collection("users")
+            .doc(uid)
+            .collection("fridges");
+          let response = [];
+
+          await query.get().then((querySnapshot) => {
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+              const selectedItem = {
+                id: doc.id,
+                name: doc.data().name,
+              };
+              response.push(selectedItem);
+            }
+            return;
+          });
+          return res.status(200).send(response);
+        })
+        .catch((error) => {
+          return res.status(401).json({ error: error });
+        });
     } catch (error) {
-      console.log(error);
       return res.status(500).send(error);
     }
   })();
 });
+
+// app.delete("/api/user/:id", (req, res) => {
+//   (async () => {
+//     try {
+//       const document = db.collection("users").doc(req.params.id);
+//       await document.delete();
+//       return res.status(200).send();
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send(error);
+//     }
+//   })();
+// });
 
 exports.app = functions.https.onRequest(app);
